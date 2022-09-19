ebefb9ef9364327eb664fd41980170ab0b118797
Olusola <81810711+Olusols@users.noreply.github.com>
2022-09-20 00:31:37 +0100

    Added some new files

--- ASP/settings.py
+++ ASP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$8yqcl#ffk7*79!+2uo$*p6&_n@5)gludua%l=+-&#w2go)u7l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['uiaspirant.herokuapp.com']
 
 
 # Application definition
